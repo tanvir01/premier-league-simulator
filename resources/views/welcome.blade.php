ab846a02e6cb7bedc488d33e0971d49eb1fe3f6a
tanvir <t.rabbi@sevensenders.com>
2022-03-30 05:30:19 +0200

    remove unnecessary stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,36 +6,12 @@
     <title>Premier League Simulator</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <script src="http://code.jquery.com/jquery-3.3.1.min.js"
-            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-            crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
             integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
             crossorigin="anonymous"></script>
-    <script src="{{asset('/js/script.js')}}"></script>
 </head>
 <body>
 <div class="container">
-
-    <div class="row">
-        <div class="alert alert-info info-box">
-            <div><strong>1 </strong> Hi, this is a four team premier cup simulator app</div>
-            <div><strong>2 </strong> you can see standings and future matches, then you can choose to simulate weekly or
-                all the weeks matches immediately
-            </div>
-            <div><strong>3 </strong> there is also a champion predictor with current team standings</div>
-            <div><strong>4 </strong> in each week you can reset all the cup and do every thing from zero</div>
-            <div><strong>5 </strong> in this league each team play 6 games including 2 times against another team, one
-                home and one away
-            </div>
-            <div><strong>6 </strong> <span class="info-box_important_note">Please note home teams with better standings rank have higher chance to win the match</span>
-            </div>
-        </div>
-    </div>
 
     @if($standings)
         <div class="row standings-box">
